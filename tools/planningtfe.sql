ebff52348828d0c43828d9ecd08ef8cff05d14e8
Pipart Denis <denis.pipart@gmail.com>
2022-09-09 20:27:41 +0200

    maj .sql

--- tools/planningtfe.sql
+++ tools/planningtfe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 24 août 2022 à 13:37
+-- Généré le : ven. 09 sep. 2022 à 18:26
 -- Version du serveur : 5.7.36
 -- Version de PHP : 8.0.0
 
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `aidepeda` (
 --
 
 INSERT INTO `aidepeda` (`pk`, `username`, `email`, `password`, `nom`, `prenom`, `session_token`, `lastlog`, `semainier_id`) VALUES
-(1, 'testUSERNAME', 'test@test.test', '$2y$10$SNuKUct6l0sRcky87vFOdevwMqIV3.8Qly2w.KtbH/coOLEgiBU4W', 'Pipart', 'Denis', 'e4e18d3235757c74.1661342844', '2022-08-24 14:07:24', 77);
+(1, 'testUSERNAME', 'test@test.test', '$2y$10$SNuKUct6l0sRcky87vFOdevwMqIV3.8Qly2w.KtbH/coOLEgiBU4W', 'Pipart', 'Denis', '474aed66d2f8348d.1662712440', '2022-09-09 10:34:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -58,18 +58,19 @@ INSERT INTO `aidepeda` (`pk`, `username`, `email`, `password`, `nom`, `prenom`, 
 DROP TABLE IF EXISTS `ecole`;
 CREATE TABLE IF NOT EXISTS `ecole` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  `adresse` varchar(255) DEFAULT NULL,
+  `nom` varchar(50) NOT NULL,
+  `adresse` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `ecole`
 --
 
 INSERT INTO `ecole` (`pk`, `nom`, `adresse`) VALUES
-(2, 'L\'Ã©cole du Bonheur', 'rue du bonheur 66, 1300 Wavre'),
-(3, 'Institut Saint Jean Baptiste', 'Wavre');
+(2, 'L\'Ã©cole du Bonheur', 'Rue du bonheur 66'),
+(3, 'Institut Saint Jean Baptiste', 'Rue Matagne 1'),
+(4, 'L\'Ã®le aux trÃ©sors', 'Avenue des moussons 5');
 
 -- --------------------------------------------------------
 
@@ -80,8 +81,8 @@ INSERT INTO `ecole` (`pk`, `nom`, `adresse`) VALUES
 DROP TABLE IF EXISTS `enfant`;
 CREATE TABLE IF NOT EXISTS `enfant` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
   `ecole_id` int(11) NOT NULL,
   `titulaire_id` int(11) NOT NULL,
   `planning_id` int(11) DEFAULT NULL,
@@ -96,12 +97,12 @@ CREATE TABLE IF NOT EXISTS `enfant` (
 --
 
 INSERT INTO `enfant` (`pk`, `nom`, `prenom`, `ecole_id`, `titulaire_id`, `planning_id`) VALUES
-(4, 'Baivier', 'Arthur', 2, 6, 41),
-(5, 'Pipart', 'Denis', 3, 5, 31),
+(4, 'Baivier', 'Arthur', 2, 6, 46),
+(5, 'Pipart', 'Denis', 3, 5, 45),
 (6, 'Dupuit', 'Bruno', 2, 6, 40),
-(7, 'Liesse', 'Laura', 2, 6, 39),
+(7, 'Liesse', 'Laura', 4, 7, 39),
 (8, 'Dupont', 'Abdel', 2, 6, 42),
-(9, 'Baivier', 'Sacha', 2, 5, 43);
+(9, 'Baivier', 'Sacha', 3, 5, 43);
 
 -- --------------------------------------------------------
 
@@ -112,25 +113,25 @@ INSERT INTO `enfant` (`pk`, `nom`, `prenom`, `ecole_id`, `titulaire_id`, `planni
 DROP TABLE IF EXISTS `planning`;
 CREATE TABLE IF NOT EXISTS `planning` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `lundi` varchar(255) NOT NULL,
-  `mardi` varchar(255) NOT NULL,
-  `mercredi` varchar(255) NOT NULL,
-  `jeudi` varchar(255) NOT NULL,
-  `vendredi` varchar(255) NOT NULL,
+  `lundi` varchar(50) NOT NULL,
+  `mardi` varchar(50) NOT NULL,
+  `mercredi` varchar(50) NOT NULL,
+  `jeudi` varchar(50) NOT NULL,
+  `vendredi` varchar(50) NOT NULL,
   PRIMARY KEY (`pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `planning`
 --
 
 INSERT INTO `planning` (`pk`, `lundi`, `mardi`, `mercredi`, `jeudi`, `vendredi`) VALUES
-(31, 'true,true,true,true,false,false', 'false,false,true,false,false,false', 'false,true,true,true', 'true,true,false,false,false,false', 'false,false,true,true,true,true'),
-(39, 'true,true,false,false,true,true', 'true,true,false,false,true,true', 'true,true,false,false', 'false,false,true,true,true,true', 'true,true,false,false,true,true'),
-(40, 'false,false,false,false,true,true', 'true,true,true,false,false,false', 'true,true,true,true', 'true,true,true,false,false,true', 'true,false,false,true,true,true'),
-(41, 'true,false,false,true,true,true', 'true,true,false,true,true,true', 'true,false,false,true', 'true,true,false,false,true,true', 'true,true,false,false,false,false'),
+(39, 'false,true,false,false,true,true', 'true,true,false,false,true,true', 'true,true,false,false', 'false,false,true,true,true,true', 'true,true,false,false,false,false'),
+(40, 'false,false,false,false,true,true', 'true,true,true,false,false,false', 'false,false,false,false', 'false,false,true,false,false,true', 'true,false,false,true,false,false'),
 (42, 'false,true,false,true,true,false', 'true,false,true,true,true,true', 'false,true,true,false', 'true,true,false,false,true,true', 'false,true,true,false,false,true'),
-(43, 'true,true,true,true,true,true', 'true,true,true,true,true,true', 'true,true,false,true', 'true,true,true,true,true,false', 'true,false,true,true,true,true');
+(43, 'true,true,true,true,true,true', 'true,true,true,true,true,true', 'true,true,false,true', 'true,true,true,true,true,false', 'true,false,true,true,true,true'),
+(45, 'false,false,true,true,false,true', 'true,true,false,true,true,false', 'true,true,true,true', 'false,false,false,true,true,false', 'false,true,false,false,false,false'),
+(46, 'true,true,true,true,true,true', 'true,true,true,true,true,true', 'true,true,true,true', 'true,true,true,true,true,true', 'true,true,true,true,true,true');
 
 -- --------------------------------------------------------
 
@@ -141,21 +142,14 @@ INSERT INTO `planning` (`pk`, `lundi`, `mardi`, `mercredi`, `jeudi`, `vendredi`)
 DROP TABLE IF EXISTS `semainier`;
 CREATE TABLE IF NOT EXISTS `semainier` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `lundi` varchar(255) NOT NULL,
-  `mardi` varchar(255) NOT NULL,
-  `mercredi` varchar(255) NOT NULL,
-  `jeudi` varchar(255) NOT NULL,
-  `vendredi` varchar(255) NOT NULL,
+  `lundi` varchar(50) NOT NULL,
+  `mardi` varchar(50) NOT NULL,
+  `mercredi` varchar(50) NOT NULL,
+  `jeudi` varchar(50) NOT NULL,
+  `vendredi` varchar(50) NOT NULL,
   `planningupdated` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `semainier`
---
-
-INSERT INTO `semainier` (`pk`, `lundi`, `mardi`, `mercredi`, `jeudi`, `vendredi`, `planningupdated`) VALUES
-(77, '5,5,4', '4,8,8', '6,6', '9,7,7', 'off,9,off', 1);
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -166,12 +160,12 @@ INSERT INTO `semainier` (`pk`, `lundi`, `mardi`, `mercredi`, `jeudi`, `vendredi`
 DROP TABLE IF EXISTS `titulaire`;
 CREATE TABLE IF NOT EXISTS `titulaire` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) DEFAULT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
   `ecole_id` int(11) NOT NULL,
   PRIMARY KEY (`pk`),
   KEY `ecole` (`ecole_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `titulaire`
@@ -179,7 +173,8 @@ CREATE TABLE IF NOT EXISTS `titulaire` (
 
 INSERT INTO `titulaire` (`pk`, `nom`, `prenom`, `ecole_id`) VALUES
 (5, 'Delvigne', 'Corentin', 2),
-(6, 'Raskin', 'Nicolas', 3);
+(6, 'Raskin', 'Nicolas', 3),
+(7, 'Dekimpe', 'FranÃ§ois', 4);
 
 --
 -- Contraintes pour les tables déchargées
